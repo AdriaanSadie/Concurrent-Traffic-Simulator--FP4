2faf97a6bae9fa06af258c2a08766674ed052879
AdriaanSadie <sadieadriaan@gmail.com>
2020-07-02 21:29:50 +0000

    Busy with FP.2a

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -45,6 +45,7 @@ void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
 }
+*/
 
 // virtual function which is executed in a thread
 void TrafficLight::cycleThroughPhases()
@@ -53,6 +54,13 @@ void TrafficLight::cycleThroughPhases()
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+  
+  	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+  
+  	while(true){
+      	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+      	
+    	std::this_thread::sleep_for(std::chrono::milliseconds(1));  
+    }
 }
 
-*/
